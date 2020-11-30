e4e0d8f6bb57a007620202de96fffc152cceae31
Alzuwayyid <m.ahmad7@hotmail.com>
2020-11-30 17:02:57 +0300

    Almofire framework was added

--- FlickerLocations/FlickerLocations/Controllers/ListViewController.swift
+++ FlickerLocations/FlickerLocations/Controllers/ListViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ListViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var collectionView: UICollectionView!
+    
+    // MARK: - Data Sources and Delegates
+    let mapViewDelegate = ListMapViewDelegate()
+    let collectionViewDataSource = ListCollectionViewDataSource()
+    let collectionViewDelegate = ListCollectionViewDelegate()
+    
+    // MARK: - Properties
     var photoFetcher = fetcher()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = mapViewDelegate
+        collectionView.dataSource = collectionViewDataSource
+        collectionView.delegate = collectionViewDelegate
         
         
         _ = URLSession.shared.dataTask(with: getFlickerURL(longitude: 46.675297, latitude: 24.713552, radius: 20, totalPagesAmount: 17, photosPerPage: 15)){
