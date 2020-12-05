c327e0d5c66b3ee6a959c90e28b6c2b2deb340de
Alzuwayyid <m.ahmad7@hotmail.com>
2020-12-05 19:46:12 +0300

    testfetchImageURLFromDiskand testFetchingImageFromTheDisk were added to testStore

--- FlickerLocations/FlickerLocationsTests/StoreTests.swift
+++ FlickerLocations/FlickerLocationsTests/StoreTests.swift
@@ -1,3 +1,4 @@
+
 //
 //  StoreTests.swift
 //  FlickerLocationsTests
@@ -7,7 +8,21 @@
 
 import Foundation
 import XCTest
+@testable import FlickerLocations
 
-class FlickerStoreTest{
+class FlickerStoreTest: XCTestCase{
+    var imageStore = ImageStore()
     
+    
+    func testfetchImageURLFromDisk(){
+        let fetchImageURL = imageStore.imageURL(forKey: "50666290098")
+        XCTAssertNotNil(fetchImageURL)
+    }
+    
+    func testFetchingImageFromTheDisk(){
+        let fetchImage = imageStore.image(forKey: "50666290098")
+        XCTAssertNotNil(fetchImage)
+    }
 }
+
+//"file:///Users/mohammed/Library/Developer/CoreSimulator/Devices/6CA0065C-E220-488F-BADE-8E8526B179BA/data/Containers/Data/Application/BCBAB2CC-A83D-47F5-93C9-A7DD62B4562A/Documents/50666290098"
