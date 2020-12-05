3827d7a9e10cf5bd46f7e111cac5a3cc4708d0b6
Alzuwayyid <m.ahmad7@hotmail.com>
2020-12-05 19:07:35 +0300

    testProccessingImage,testLocationOfImage methods were added to ApiTest

--- FlickerLocations/FlickerLocationsTests/APITests.swift
+++ FlickerLocations/FlickerLocationsTests/APITests.swift
@@ -5,9 +5,47 @@
 //  Created by Mohammed on 05/12/2020.
 //
 
-import Foundation
 import XCTest
+@testable import FlickerLocations
 
-class FlickerApiTest{
+class FlickerApiTest: XCTestCase{
+    var photoFetcher = fetcher()
+    var constants = Constants()
+    var customError = CustomError(title: nil, description: "NA", code: 404)
+    
+    override func setUp(){
+        super.setUp()
+        // Do stuff
+    }
+    
+    override func tearDown() {
+        // Do stuff
+        super.tearDown()
+    }
+    
+    func testProccessingImage(){
+ 
+        let data = Data()
+        
+        let photo = photoFetcher.processImageRequest(data: data, error: customError)
+        XCTAssertNotNil(photo)
+        XCTAssertEqual(OperationQueue.current, OperationQueue.main)
+    }
+    
+    func testLocationOfImage(){
+        
+        let completionExpectation = expectation(description: "Execute completion closure.")
+        
+        let _: () = photoFetcher.fetchPhotosLocation(url: (getphotoLocationURL(photoId: 50666290098))) { (location, error) in
+            XCTAssertEqual(location?.country.content, "Saudi Arabia")
+            XCTAssertEqual(OperationQueue.current,
+                           OperationQueue.main,
+                           "Completion handler should run on the main thread; it did not.")
+            completionExpectation.fulfill()
+
+        }
+        waitForExpectations(timeout: 1.0, handler: nil)
+
+    }
     
 }
